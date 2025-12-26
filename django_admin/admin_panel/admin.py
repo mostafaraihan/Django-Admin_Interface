6d5448cc6c794fb0378b1fb59111ecbc5032d068
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-26 22:17:51 +0600

    admin add model

--- django_admin/admin_panel/admin.py
+++ django_admin/admin_panel/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'roll_no')
+    search_fields = ('roll_no',)
+    list_filter = ('roll_no',)
+    list_per_page = 10
+    list_max_show_all = 20
+
+
+admin.site.register(Student, StudentAdmin)
