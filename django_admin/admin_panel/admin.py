952ff7a302957a615c97040fa66b9f651b5b2bd2
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-26 22:23:27 +0600

    modify

--- django_admin/admin_panel/admin.py
+++ django_admin/admin_panel/admin.py
@@ -3,11 +3,9 @@ from .models import Student
 
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'roll_no')
-    search_fields = ('roll_no',)
-    list_filter = ('roll_no',)
+    list_display = ('Name', 'Roll')
+    search_fields = ('Roll',)
+    list_filter = ('Roll',)
     list_per_page = 10
-    list_max_show_all = 20
-
 
 admin.site.register(Student, StudentAdmin)
--- django_admin/admin_panel/models.py
+++ django_admin/admin_panel/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    father_name = models.CharField(max_length=100)
-    mother_name = models.CharField(max_length=100)
-    roll_no = models.CharField(max_length=100)
-    technology = models.CharField(max_length=100)
-    address = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=15)
+    Name = models.CharField(max_length=100)
+    Age = models.IntegerField()
+    Father_name = models.CharField(max_length=100)
+    Mother_name = models.CharField(max_length=100)
+    Roll= models.CharField(max_length=100, primary_key=True)
+    Technology = models.CharField(max_length=100)
+    Address = models.CharField(max_length=255)
+    Mobile = models.CharField(max_length=15)
