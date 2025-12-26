33bacc5ad621dc6449529730afc1598403e9726b
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-26 22:28:42 +0600

    admin change

--- django_admin/admin_panel/admin.py
+++ django_admin/admin_panel/admin.py
@@ -3,7 +3,7 @@ from .models import Student
 
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Roll')
+    list_display = ('Name', 'Roll', 'Technology')
     search_fields = ('Roll',)
     list_filter = ('Roll',)
     list_per_page = 10
