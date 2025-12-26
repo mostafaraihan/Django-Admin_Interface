86ac324f612703e68dae51cddc8dd740affda5d7
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-26 22:03:36 +0600

    model create

--- django_admin/admin_panel/models.py
+++ django_admin/admin_panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    father_name = models.CharField(max_length=100)
+    mother_name = models.CharField(max_length=100)
+    roll_no = models.CharField(max_length=100)
+    technology = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=15)
