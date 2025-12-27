369e321283c8fe20501001681d05a88859f1bc02
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-27 11:12:08 +0600

    list display

--- django_admin/admin_panel/admin.py
+++ django_admin/admin_panel/admin.py
@@ -4,8 +4,5 @@ from .models import Student
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('Name', 'Roll', 'Technology')
-    search_fields = ('Roll',)
-    list_filter = ('Roll',)
-    list_per_page = 10
 
 admin.site.register(Student, StudentAdmin)
