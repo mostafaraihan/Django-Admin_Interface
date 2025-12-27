2480e1aea05df14f265f3e0e092efd1ffe9b9668
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-27 11:13:56 +0600

    search field

--- django_admin/admin_panel/admin.py
+++ django_admin/admin_panel/admin.py
@@ -3,6 +3,11 @@ from .models import Student
 
 
 class StudentAdmin(admin.ModelAdmin):
+    #Fields Display
     list_display = ('Name', 'Roll', 'Technology')
+
+    #Search Field
+    search_fields = ('Name','Roll')
+
 
 admin.site.register(Student, StudentAdmin)
